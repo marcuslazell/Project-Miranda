4a3685fcd47ce3c5ee188184d210f39c37763d51
Marcus <marcus@marcuslshaw.com>
2024-12-11 11:05:46 -0800

    Initial Commit

---
+++ Project Miranda/Project_MirandaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_MirandaApp.swift
+//  Project Miranda
+//
+//  Created by Marcus Shaw II on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_MirandaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project MirandaTests/Project_MirandaTests.swift
@@ -0,0 +1,16 @@
+//
+//  Project_MirandaTests.swift
+//  Project MirandaTests
+//
+//  Created by Marcus Shaw II on 12/11/24.
+//
+
+import Testing
+
+struct Project_MirandaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
